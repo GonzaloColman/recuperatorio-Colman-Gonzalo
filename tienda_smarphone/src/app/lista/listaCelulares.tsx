aaed7894ee47862e7b9787ef66d292be6e8bd836
gonzalo <gonzalogabrielapu@gmail.com>
2024-02-15 13:37:13 -0300

    formato

--- tienda_smarphone/src/app/lista/listaCelulares.tsx
+++ tienda_smarphone/src/app/lista/listaCelulares.tsx
@@ -1,8 +1,16 @@
-"use client"
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export const Lista = () => {
   return (
-    <div></div>
+    <div className='lista'>
+      <ListGroup >
+        <ListGroup.Item>celu1</ListGroup.Item>
+        <ListGroup.Item>celu1</ListGroup.Item>
+        <ListGroup.Item>celu3</ListGroup.Item>
+        <ListGroup.Item>celu4</ListGroup.Item>
+      </ListGroup>
+    </div>
   )
 }
