e078390efbb475e6bb9f75c1ecf6e4cb7a8104de
gonzalo <gonzalogabrielapu@gmail.com>
2024-02-16 16:59:22 -0300

    correcciones

--- tienda_smarphone/src/app/encabezados_PieDePagina/encabezado.tsx
+++ tienda_smarphone/src/app/encabezados_PieDePagina/encabezado.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 export const Encabezado = () => {
   return (
-      <header>
+      <div >
+        <div className='titulo'>
         <h1>CeluMundo</h1>
         <h4>Tienda de ventas de smartphones</h4>
-      </header>
+      </div>
+        
+      </div>
   )
 }
--- tienda_smarphone/src/app/encabezados_PieDePagina/pieDePagina.tsx
+++ tienda_smarphone/src/app/encabezados_PieDePagina/pieDePagina.tsx
@@ -5,9 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const PieDePagina = () => {
   return (
     <footer>
-      <div /*className="container"*/>
-        <p /*className="float-start"*/> Contacto Telefonico: </p>
-        <p /*className="float-end"*/>Correo:<a className="link-opacity-50-hover"
+      <div className="container">
+        <p className="float-start"> Contacto Telefonico: </p>
+        <p className="float-end">Correo:<a className="link-opacity-50-hover"
           href="" target="blank"></a></p>
       </div>
     </footer>
