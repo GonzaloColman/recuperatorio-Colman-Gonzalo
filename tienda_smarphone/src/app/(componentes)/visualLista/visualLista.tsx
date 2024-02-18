07fb44604f0e1017bc3b56f29d6699af05e1a9fc
gonzalo <gonzalogabrielapu@gmail.com>
2024-02-18 19:44:13 -0300

    diseño de lista

---
+++ tienda_smarphone/src/app/(componentes)/visualLista/visualLista.tsx
@@ -0,0 +1,24 @@
+import Card from 'react-bootstrap/Card';
+import Celu1 from '../../../../public/celu1.webp';
+
+export const ListaVisual = () => {
+  return (
+    <Card style={{ width: '36rem', display: 'flex', flexDirection: 'row' }}>
+      <Card.Img variant="top" src={Celu1.src} style={{ width: '30%' }} />
+      <div style={{ width: '80%' }}>
+        <Card.Body>
+          <Card.Title>Xiaomi Redmi Note 10</Card.Title>
+          <Card.Text>
+            $450.000
+          </Card.Text>
+          <Card.Text>
+            6 cuotas sin interes
+          </Card.Text>
+          <Card.Text>
+            Envio Gratis
+          </Card.Text>
+        </Card.Body>
+      </div>
+    </Card>
+  );
+}
--- tienda_smarphone/src/app/lista/listaCelulares.tsx
+++ tienda_smarphone/src/app/lista/listaCelulares.tsx
@@ -2,15 +2,16 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ListGroup from 'react-bootstrap/ListGroup';
+import {ListaVisual} from '../(componentes)/visualLista/visualLista'
 
 export const Lista = () => {
   return (
     <div className='lista'>
       <ListGroup >
-        <ListGroup.Item>celu1</ListGroup.Item>
-        <ListGroup.Item>celu1</ListGroup.Item>
-        <ListGroup.Item>celu3</ListGroup.Item>
-        <ListGroup.Item>celu4</ListGroup.Item>
+        <ListGroup.Item>celu3  </ListGroup.Item>
+        <ListGroup.Item><ListaVisual/></ListGroup.Item>
+        <ListGroup.Item><ListaVisual/></ListGroup.Item>
+        <ListGroup.Item><ListaVisual/></ListGroup.Item>
       </ListGroup>
     </div>
   )
