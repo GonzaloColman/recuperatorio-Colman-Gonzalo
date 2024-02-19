de15c2afdad33bdbe8520975786d00d1458ab5ed
gonzalo <gonzalogabrielapu@gmail.com>
2024-02-19 17:08:42 -0300

    menuLateral

---
+++ tienda_smarphone/src/app/(componentes)/menuLateral/MenuLateral.tsx
@@ -0,0 +1,14 @@
+"user client"
+import React from 'react'
+
+export const MenuLateral = () => {
+  return (
+    <div className='menu'>
+      <a >Marca</a>
+      <a>Sistema operativo</a>
+      <a>camara</a>
+      <a>bateria</a>
+      <a>Envio Gratis</a>
+    </div>
+  );
+}
--- tienda_smarphone/src/app/page.tsx
+++ tienda_smarphone/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Encabezado } from "./encabezados_PieDePagina/encabezado";
 import { Lista } from "./lista/listaCelulares";
 import { PieDePagina } from "./encabezados_PieDePagina/pieDePagina";
+import { MenuLateral } from "./(componentes)/menuLateral/MenuLateral";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Home() {
   return (
     <main>
       <Encabezado/>
+      <MenuLateral/>
       <Lista/>
       <PieDePagina/>
     </main>
